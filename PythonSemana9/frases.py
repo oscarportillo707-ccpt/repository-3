ace3f2a859772f72f55649852cfd852958562032
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-03-21 13:09:43 -0600

    actualización

---
+++ PythonSemana9/frases.py
@@ -0,0 +1,11 @@
+# Solicitar una frase
+frase = input("Ingresa una frase: ")
+
+# Mostrar la frase original
+print("Frase original:", frase)
+
+# Mostrar en mayúsculas
+print("Frase en mayúsculas:", frase.upper())
+
+# Mostrar en minúsculas
+print("Frase en minúsculas:", frase.lower())
