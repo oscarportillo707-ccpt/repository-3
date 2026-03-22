58598e54e89a3662468672a609c1feb565ae2857
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-03-21 22:23:57 -0600

    cambios

---
+++ PythonSemana9/nombres.py
@@ -0,0 +1,11 @@
+# Solicitar el nombre completo
+nombre = input("Ingresa tu nombre completo: ")
+
+# Mostrar en mayúsculas
+print("Nombre en mayúsculas:", nombre.upper())
+
+# Mostrar en minúsculas
+print("Nombre en minúsculas:", nombre.lower())
+
+# Cantidad de caracteres a mostrar
+print("Cantidad de caracteres:", len(nombre))
