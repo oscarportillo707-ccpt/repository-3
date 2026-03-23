3e015792c885b020c6cec2ee3dac2c28c6cbcb66
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-03-22 19:32:21 -0600

    se realiza una modificación

--- PythonSemana9/nombres.py
+++ PythonSemana9/nombres.py
@@ -4,8 +4,14 @@ nombre = input("Ingresa tu nombre completo: ")
 # Mostrar en mayúsculas
 print("Nombre en mayúsculas:", nombre.upper())
 
+## upper convierte a mayusculas
+
 # Mostrar en minúsculas
 print("Nombre en minúsculas:", nombre.lower())
 
+## lower a minusculas
+
 # Cantidad de caracteres a mostrar
 print("Cantidad de caracteres:", len(nombre))
+
+## cuentas los caracteres del texto
