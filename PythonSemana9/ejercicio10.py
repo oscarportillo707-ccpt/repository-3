32d3d94b03a547b8b23c2adfc340cb0103948693
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-03-22 19:42:19 -0600

    se agrega ejercicio 10

---
+++ PythonSemana9/ejercicio10.py
@@ -0,0 +1,10 @@
+frase = input("Ingresa una frase: ")
+
+# Verificar si termina con punto
+if frase.endswith("."):
+
+    ## el endswitch sirve para verificar si la frase o palabra termina con algo especifico
+
+    print("La frase termina con un punto")
+else:
+    print("La frase no termina con un punto")
