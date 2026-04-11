48976a9b1908c7534a5317ee5bb6844db80202dc
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-04-11 17:25:39 -0600

    Parcial 02, Fundamentos de programación, ejercicio 3, remover prefijos y convertir todo a mayusculas utilizando removeprefix y upper

---
+++ PARCIAL-02/pEjercicio3.py
@@ -0,0 +1,8 @@
+texto = "ING. Oscar Javier Portillo"
+
+texto_sin_prefijo = texto.removeprefix("ING. ")
+
+texto_mayusculas = texto_sin_prefijo.upper()
+
+print("Texto sin prefijo:", texto_sin_prefijo)
+print("Texto en mayusculas:", texto_mayusculas)
