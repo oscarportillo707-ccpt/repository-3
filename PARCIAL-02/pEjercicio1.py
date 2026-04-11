8e82728ab11570fa063ea62694f66fd04ca6371c
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-04-11 17:21:14 -0600

    Parcial 02, Fundamentos de programación, ejercicio 1, uso de los metodos strip y count

---
+++ PARCIAL-02/pEjercicio1.py
@@ -0,0 +1,9 @@
+# Declarar la variable
+texto = "  elefante  "
+
+textoLimpio = texto.strip()
+
+cantidad_e = textoLimpio.count("e")
+
+print("Texto sin espacios:", textoLimpio)
+print("Cantidad de 'e':", cantidad_e)
