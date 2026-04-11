45227a0ed33e4d124c9dc92e14644e998f78ef0d
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-04-11 17:27:43 -0600

    Parcial 02, Fundamentos de programción, ejercicio 4, uso de los metodos lower, removesuffix y find

---
+++ PARCIAL-02/pEjercicio4.py
@@ -0,0 +1,11 @@
+texto = "CANTANDO"
+
+texMinuscula = texto.lower()
+
+texto_sin_sufijo = texMinuscula.removesuffix("ando")
+
+indice_t = texto_sin_sufijo.find("t")
+
+print("Texto en minuscula:", texMinuscula)
+print("Texto sin sufijo:", texto_sin_sufijo)
+print("Índice de 't':", indice_t)
