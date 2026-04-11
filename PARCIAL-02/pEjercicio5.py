c21698cca85eec3fa932c6160a52087d9ccdaa3a
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-04-11 17:29:13 -0600

    Parcial 02, Fundamentos de programación, ejercicio 5, uso de swapcase y Ijust

---
+++ PARCIAL-02/pEjercicio5.py
@@ -0,0 +1,10 @@
+# Cadena inicial
+texto = "pYTHON"
+
+texto_invertido = texto.swapcase()
+
+texto_final = texto_invertido.ljust(15, "*")
+
+# Mostrar resultado
+print("Letras invertidas:", texto_invertido)
+print("Texto alineado:", texto_final)
