77b84f97426ce20bc0392310b013c30e781c65de
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-04-18 21:32:25 -0600

    se agrega el ejercicio 10 de la tarea

---
+++ Semana-12/Ejercicios/Ejercicio10.py
@@ -0,0 +1,8 @@
+usuario = input("Ingresa tu nombre de usuario: ")
+
+contraseña = input("Ingresa tu contraseña: ")
+
+if usuario == "oscarin" and contraseña == "1234":
+    print("¡Acceso Permitido!")
+else:
+    print("¡Acceso Denegado!")
