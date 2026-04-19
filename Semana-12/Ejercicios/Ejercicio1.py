c2808c64023e1afb2d915c0c3c17d5f15010ae83
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-04-18 21:25:53 -0600

    se agrega el ejercicio 1 de la tarea

---
+++ Semana-12/Ejercicios/Ejercicio1.py
@@ -0,0 +1,8 @@
+numUsuario = int(input("Ingrese un número: "))
+
+if numUsuario > 0:
+    print("El número es positivo.")
+elif numUsuario < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
