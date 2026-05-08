5fcb08bbeb1fc46bd022f5752a5e43d978fb563a
oscarportillo707-ccpt <pt0460032026@unab.edu.sv>
2026-05-07 21:41:59 -0600

    uso de los metodos arrays

---
+++ Semana15/métodosArrays.py
@@ -0,0 +1,23 @@
+series = []
+
+series.append("Naruto")
+series.append("One Piece")
+series.append("Dragon Ball")
+series.append("Bleach")
+series.append("Death Note")
+series.append("Attack on Titan")
+series.append("Fullmetal Alchemist")
+series.append("My Hero Academia")
+
+# para agregar un elemento a un arrays es necesario utilizar appen()
+# seres.appen("")
+# series.insert(1, "Patrick")
+# series.remove("Patrick")
+# series.pop(8)
+
+series.insert(2, "One Punch Man")
+series.insert(0, "Demon Slayer")
+series.insert(5, "Tokyo Ghoul")
+series.pop(3)
+series.remove("Death Note")
+print(series)
